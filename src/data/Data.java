6decbb3e87a85bfe210ed359465c42314d51dee2
Petersg1 <petersg1@hotmail.fr>
2015-11-10 14:56:54 +0100

    Envoie en broadcast + met la bonne ip source

--- src/data/Data.java
+++ src/data/Data.java
@@ -1,7 +1,10 @@
 package data;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 /**
  * Created by pierre on 09/11/15.
@@ -13,12 +16,45 @@ public class Data {
     private LocalUser localUser;
 
     /* Constructeur */
-    public Data() throws UnknownHostException {
+    public Data() throws UnknownHostException, SocketException {
         this.usersList = new UsersList();
-        this.localUser = new LocalUser("", InetAddress.getLocalHost());
+
+        String ip;
+        InetAddress addr = InetAddress.getLocalHost();
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                // filters out 127.0.0.1 and inactive interfaces
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    addr = addresses.nextElement();
+                    ip = addr.getHostAddress();
+                    System.out.println(iface.getDisplayName() + " " + ip);
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (addr == InetAddress.getLocalHost()) {
+            System.out.println("/!\\ Adresse local : " + addr);
+        } else {
+            this.localUser = new LocalUser("Pierre", addr);
+            System.out.println("/!\\ Adresse utilisée : " + addr);
+        }
+
     }
 
     /* Methods */
+
+    public LocalUser getLocalUser() {
+        return localUser;
+    }
+
     public void addUser(String nickname, InetAddress ip) {
         this.usersList.add(nickname, ip);
     }
@@ -26,16 +62,11 @@ public class Data {
         this.usersList.add(user);
     }
 
-    public void removeUser(User user) {
+    public void remove(User user) {
         this.usersList.remove(user);
     }
-    public void removeUser(InetAddress ip){this.usersList.remove(ip);}
 
     public Boolean userConnected() {
         return localUser.getConnected();
-    }
-
-    public LocalUser getLocalUser() {
-        return localUser;
     }
 }
--- src/network/SendController.java
+++ src/network/SendController.java
@@ -1,6 +1,8 @@
 package network;
 
+import packet.Bye;
 import packet.Hello;
+import packet.Packet;
 import packet.SerDeser;
 
 import java.io.*;
@@ -13,17 +15,26 @@ import java.net.InetAddress;
  */
 public class SendController {
 
-    //Envoie un hello à tous <3 enfin au localhost pour l'instant
-    public void sHello(Hello helloPacket) throws IOException {
-
-        byte[] byteHello = SerDeser.serialize(helloPacket);
+    //Envoie un packet en broadcast sur 42025
+    public void sendBroadcast(Packet packet) throws IOException {
+        byte[] bytePacket = SerDeser.serialize(packet);
         DatagramSocket udpSocket = new DatagramSocket();
-        DatagramPacket udpPacket = new DatagramPacket(byteHello,byteHello.length,InetAddress.getByName("255.255.255.255"), 42025 );
+        DatagramPacket udpPacket = new DatagramPacket(bytePacket,bytePacket.length,InetAddress.getByName("255.255.255.255"), 42025 );
 
         udpSocket.setBroadcast(true);
         udpSocket.send(udpPacket);
         udpSocket.close();
 
-        System.out.println("Hi, just sent the packet to " + InetAddress.getLoopbackAddress());
+        System.out.println("Hi, just sent the packet to my local network");
+    }
+
+    //Envoie un hello à tous <3 enfin au localhost pour l'instant
+    public void sHello(Hello helloPacket) throws IOException {
+        sendBroadcast(helloPacket);
+    }
+
+    //Envoie un bye à tous <3 enfin au localhost pour l'instant
+    public void sBye(Bye byePacket) throws IOException {
+        sendBroadcast(byePacket);
     }
 }
